fab67aca6ab0aad5d0598d286fd547e668bec923
Adán López <adanl@uninorte.edu.co>
2020-08-25 10:41:28 -0500

    Assignment solved

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function stores some functions in the parent environment.
+##
 
 makeCacheMatrix <- function(x = matrix()) {
-
+     i<- NULL
+     set <- function(y) {
+                    x <<- y
+                    i <<- NULL
+          }
+             get <- function() x
+           setinv <- function(solve) i <<- solve
+          getinv <- function() i
+          list(set = set, get = get,
+                   setinv = setinv,
+                  getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function should check if the inverse of a matrix was already found
+## In case it wasn't, it finds its inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     i <- x$getinv()
+        if(!is.null(i)) {
+            message("getting cached data")
+          return(i)
+     }
+     data <- x$get()
+         i <- solve(data, ...)
+          x$setinv(i)
+        i
 }
